e2c602c9343c24c3ac54ea3a809ff5169b24d063
Stellarend <andrevallestero@gmail.com>
2018-03-08 09:59:04 -0500

    Added test movement and fixed code structure

--- Alpha/Pixel Art Printer 1.00.py
+++ Alpha/Pixel Art Printer 1.00.py
@@ -5,8 +5,8 @@
 import win32api, win32con, time
 from PIL import Image
 
-# the time to hold down keys, default = 0.05, max should be 0.17 which is 1 frame
-holdTime = 0.05
+# the time to hold down keys, default = 0.005, max should be 0.017 which is 1 frame
+holdTime = 0.005
 # the index of the currently selected toolbar with range [0,8]
 selectedToolbar = 0
 # the index of the currently selected block with range [0, 7]
@@ -33,8 +33,14 @@ def press_key(vkCode):
 
 def release_key(vkCode):
     win32api.keybd_event(vkCode,0,win32con.KEYEVENTF_KEYUP,0)
+
+def place_block():
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,0,0,0)
+    time.sleep(holdTime)
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,0,0,0)
+    time.sleep(holdTime)
     
-def place_block(targetColor):
+def select_block(targetColor):
     # try to find the block with the most similar color
     bestDifference = 9999
     bestToolbar = -1
@@ -50,6 +56,7 @@ def place_block(targetColor):
 
     # select the appropriate toolbar if not already selected
     if bestToolbar != selectedToolbar:
+        global selectedToolbar
         selectedToolbar = bestToolbar
         # hold down x to load toolbars
         press_key(0x58)
@@ -62,16 +69,31 @@ def place_block(targetColor):
     
     # select the appropriate block if not already selected
     if bestBlock != selectedBlock:
+        global selectedBlock
         selectedBlock = bestBlock
         press_key(keyMap[selectedBlock])
         time.sleep(holdTime)
         release_key(keyMap[selectedBlock])
 
-    # place the block
-    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,0,0,0)
-    time.sleep(holdTime)
-    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,0,0,0)
+for i in range(5, 0, -1):
+    print("Script will run in {}".format(i))
+    time.sleep(1)
 
-    
+imgHeight = 1
+for i in range(imgHeight, 0, -1):
+    x = 0.3
+    startTick = time.perf_counter()
+    # hold "d" key to strafe right
+    press_key(0x44)
+    #while(x < 19.7): #whatever the max width is
+    while(time.perf_counter() - startTick < 5):
+        place_block()
+    release_key(0x44)
+
+    # vk code for "a" key is 0x41
+
+#when above the middle of the block, switch to the next one
+#since the block below should have been placed already so
+#player can spam click right and place when reach the next block
 
 input("Press enter key to exit . . . ")
